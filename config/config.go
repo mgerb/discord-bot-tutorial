7e4027ddc90d68ccca4f958c9c18fe701e8a9399
mgerb42 <mgerb42@gmail.com>
2017-02-16 21:30:53 +0000

    Part 2

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+var (
+	// Public variables
+	Token     string
+	BotPrefix string
+
+	// Private variables
+	config *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"Token"`
+	BotPrefix string `json:"BotPrefix"`
+}
+
+func ReadConfig() error {
+	fmt.Println("Reading config file...")
+
+	file, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println(string(file))
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,52 +1,21 @@
 package main
 
 import (
+	"./bot"
+	"./config"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
 )
 
-const token string = ""
-
-var BotID string
-
 func main() {
-	dg, err := discordgo.New("Bot " + token)
+	err := config.ReadConfig()
 
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	u, err := dg.User("@me")
-
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	BotID = u.ID
-
-	dg.AddHandler(messageHandler)
-
-	err = dg.Open()
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	fmt.Println("Bot is running!")
+	bot.Start()
 
 	<-make(chan struct{})
 	return
-}
-
-func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	if m.Author.ID == BotID {
-		return
-	}
-
-	if m.Content == "ping" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
-	}
 }
